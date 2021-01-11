99820e90986ae0a5317da2a7e353673b204494c9
Atri Hegde <iamatrihegde@gmail.com>
2021-01-11 10:38:51 +0000

    Final

--- Main.py
+++ Main.py
@@ -1,20 +1,22 @@
 import random
 
 
-def calculate_pi(n):
+def calculate_pi(i):
+
     num_point_circle = 0
     num_point_total = 0
-    for _ in range(n):
+
+    for _ in range(i):
         x = random.uniform(0,1)
         y = random.uniform(0,1)
-        distance = x**2 + y**2
+        distance = x**2 + y**2 #distance between the origin and the coordinate x,y
 
-        if distance <= 1:
+        if distance <= 1: #if it is inside the radius of the circle or outside.
             num_point_circle += 1
 
         num_point_total += 1
 
-    return 4 * (num_point_circle / num_point_total)
+    return 4 * (num_point_circle / num_point_total) #re arrangement of the  a=pi * r **2 formula to give us the value of pi
 
 
 print(calculate_pi(10_000))
